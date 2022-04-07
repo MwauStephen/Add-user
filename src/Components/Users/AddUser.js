a8221176237cb0d27697abeeb998ba6518485472
Mwau stephen <stephenmwau41@gmail.com>
2022-04-07 21:38:32 +0800

    added state to listen to user inputs for the name input and imported states

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const addNameHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <Card className={styles.input}>
       <form>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" />
+        <input type="text" id="username" onChange={addNameHandler} />
         <label htmlFor="age">Age(Years):</label>
         <input type="number" id="age" />
       </form>
