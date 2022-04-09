9ab52fbb57a79c198871593a2184c7834af9ef2d
Mwau stephen <stephenmwau41@gmail.com>
2022-04-09 10:13:31 +0800

    added un ordered list

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -2,7 +2,11 @@ import React from "react";
 import Card from "../UI/Card";
 
 const UsersList = (props) => {
-  return <Card></Card>;
+  return (
+    <Card>
+      <ul></ul>
+    </Card>
+  );
 };
 
 export default UsersList;
