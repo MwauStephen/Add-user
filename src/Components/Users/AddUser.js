73c4b5d469c1f3abb2c84d3e2b0c91f027aa760f
Mwau stephen <stephenmwau41@gmail.com>
2022-04-09 20:04:24 +0800

    added the button inside the form

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -7,21 +7,30 @@ const AddUser = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    // console.log(enteredName, enteredAge);
+  };
+
   const addNameHandler = (event) => {
     console.log(event.target.value);
+    // setEnteredName(event.target.value);
   };
   const addAgeHandler = (event) => {
     console.log(event.target.value);
+    // setEnteredAge(event.target.value);
   };
+
   return (
     <Card className={styles.input}>
-      <form onSubmit={addFormHandler}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username:</label>
         <input type="text" id="username" onChange={addNameHandler} />
         <label htmlFor="age">Age(Years):</label>
         <input type="number" id="age" onChange={addAgeHandler} />
+        <Button type="submit">Add user</Button>
       </form>
-      <Button>Add user</Button>
     </Card>
   );
 };
