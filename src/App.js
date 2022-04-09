33030f01b7b64c31b89b911d85167362e544ee35
Mwau stephen <stephenmwau41@gmail.com>
2022-04-09 22:34:36 +0800

    lifted the user name and age inputs using an handler passed to the AddUser component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import AddUser from "./Components/Users/AddUser";
 import UsersList from "./Components/Users/UsersList";
 
 function App() {
-  // 5.adde user inputs to an array using state
+  // 5.add user inputs to an array using state
   const [userList, setUserList] = useState([]);
+
+  // 6.Lifting state up in parent
+  const addUserLiftHandler=(upliftedUserName,upliftedUserAge)=>{
+    setUserList((prevUserList)=>{
+      return [...prevUserList,{
+        name:upliftedUserName,
+        age:upliftedUserAge
+      }]
+    })
+      
+    }
+  }
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserLiftHandler} />
       <UsersList users={userList} />
     </div>
   );
