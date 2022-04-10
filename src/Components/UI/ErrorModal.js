96ed7a6fe780f6e3e87d24027e80ef85c56b585e
Mwau stephen <stephenmwau41@gmail.com>
2022-04-10 13:15:52 +0800

    added classNames to the jsx elements and imported the Card component

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,18 +1,22 @@
 import React from "react";
 import Button from "./Button";
+import Card from "./Card";
+import styles from "./ErrorModal.module.css";
 
 const ErrorModal = () => {
   return (
     <div>
-      <header>
-        <h2>Invalid input</h2>
-      </header>
-      <div>
-        <p>Please enter a valid input</p>
-      </div>
-      <footer>
-        <Button>Okay</Button>
-      </footer>
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>Invalid input</h2>
+        </header>
+        <div className={styles.content}>
+          <p>Please enter a valid input</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
     </div>
   );
 };
